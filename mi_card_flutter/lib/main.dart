2ec8a78046a1e5ba847ec6c87f8ef5f5ad614ca7
yeongje <mykoji12@naver.com>
2020-03-16 21:17:44 +0900

    [0316] topic 45 & 46
    45: How to use Column & Row Widgets for Layout
    46: Flutter Layouts Challenge

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,21 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                    ),
+                    child: Text("Container 3"),
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                    ),
+                    child: Text("Container 3"),
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
