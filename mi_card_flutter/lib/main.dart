cad846f9d0d9bf101b96e6887b5bdbac4a46e5d4
yeongje <mykoji12@naver.com>
2020-03-17 20:44:10 +0900

    [0317] topic 47, 48 & 49
    47: Tapping into Widget Properties
    48: Incorporating Custom Fonts in your Flutter App
    49: Adding Material Icons with the Icon Widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -12,41 +12,80 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/yeongje.jpeg'),
+            ),
+            Text(
+              'YeongJe Park',
+              style: TextStyle(
+                fontFamily: 'Monoton',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
                 children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                    ),
-                    child: Text("Container 3"),
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                    ),
-                    child: Text("Container 3"),
+                  SizedBox(
+                    width: 10.0,
                   ),
+                  Text(
+                    '+82 10 3923 5866',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  )
                 ],
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'pyeongje@gamil.com',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro'),
+                  )
+                ],
               ),
-            ],
-          ),
-        ),
+            )
+          ],
+        )),
       ),
     );
   }
