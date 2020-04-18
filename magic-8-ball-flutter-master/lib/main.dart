2bca73aa5489f4f5bab87849a29daf82b22c6b6f
yeongje <mykoji12@naver.com>
2020-04-18 21:16:06 +0900

    [0418] Boss Level Challenge 1 - Magic 8 Ball
    Topic 70 ~ 78 : Nothing Speical...

---
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int randomNumber = 1;
+
+  void changeNumber() {
+    randomNumber = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            changeNumber();
+          });
+          print('I got clicked. $randomNumber');
+        },
+        child: Image.asset('images/ball$randomNumber.png'),
+      ),
+    );
+  }
+}
